938f53e08044c77cfd336b87859128ea09860fac
Gabriel Wallin <walling@student.chalmers.se>
2019-02-25 15:54:15 +0100

    fix merge conflict

--- src/index.jsx
+++ src/index.jsx
@@ -9,14 +9,9 @@ class App extends React.Component {
     render() {
         return (
             <div className="container">
-<<<<<<< HEAD
                 <div className="itemHeader"> <Header/> </div>
                 <div className="itemSidebar">This is a sidebar!</div>
                 <div className="itemContent"><Board/></div>
-=======
-                <Header/>
-                <Board/>
->>>>>>> e15e1ea6285c60316fd1b57f85a62cc300ebe585
             </div>
             
         );
