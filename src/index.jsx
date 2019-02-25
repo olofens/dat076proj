2f44e7b73fba0621a6477e63dd7c126e8d1c70af
Strid <stride@student.chalmers.se>
2019-02-25 14:02:40 +0100

    test

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render() {
         return (
-            <div>Hello!</div>
+            <div>Hello123!</div>
         );
     }
 }
