e15e1ea6285c60316fd1b57f85a62cc300ebe585
Olof Enström <olofens@student.chalmers.se>
2019-02-25 15:44:07 +0100

    board

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from "./header.jsx";
+import Board from "./board.jsx"
 import './test.css';
 
 
 class App extends React.Component {
     render() {
         return (
-            <div class="container">
+            <div className="container">
                 <Header/>
-                <div>Hello123!</div>
+                <Board/>
             </div>
             
         );
