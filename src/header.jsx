2c3ab08baed0484ceaa4d12f2bf33b17c206640d
Gabriel Wallin <walling@student.chalmers.se>
2019-02-25 16:23:10 +0100

    add header design for buttons and text

--- src/header.jsx
+++ src/header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./header.css";
 
 
 class Header extends React.Component {
     render() {
         return (
-            <div>
-                <div>This is our Header!</div>
+            <div className="headerContainer">
+                <div className="titleText">Eriks tid tagar ur!</div>
+                <div className="recordButton"><button>Record</button></div>
+                <div className="stopButton"><button>Stop</button></div>
             </div>
         );
     }
