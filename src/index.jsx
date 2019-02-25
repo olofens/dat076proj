7841d3124073b7e87abd6f9af7418eaf4e6d70f3
Gabriel Wallin <walling@student.chalmers.se>
2019-02-25 15:33:25 +0100

    fix css with webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from "./header.jsx";
+import './test.css';
 
 
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div class="container">
                 <Header/>
                 <div>Hello123!</div>
             </div>
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,11 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
-            }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ]
     },
     output: {
