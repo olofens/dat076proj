4dba0e25c28213a41cda2bcbdcf8d1dd875d3367
olofens <olofens@student.chalmers.se>
2019-03-04 11:51:01 +0100

    timertesting example2

--- src/components/middleTask.jsx
+++ src/components/middleTask.jsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom";
 class MiddleTask extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            time: 0
+        }
         this.clickedBack = this.clickedBack.bind(this);
         this.clickedForward = this.clickedForward.bind(this);
+        this.startTimer = this.startTimer.bind(this);
     }
 
     clickedBack() {
@@ -15,6 +18,16 @@ class MiddleTask extends React.Component {
 
     clickedForward() {
         this.props.actionForward(this.props.task);
+    }
+
+    startTimer() {
+        this.timer = setInterval(() => {
+            this.setState({
+                time: this.state.time + 1
+            });
+            console.log("counting to", this.state.time);
+        }, 1000);
+        console.log("start")
     }
 
     render() {
@@ -37,6 +50,10 @@ class MiddleTask extends React.Component {
                             <button onClick={this.clickedBack}>Pause</button>
                             <button onClick={this.clickedForward}>Done</button>
                         </td>
+                        <td>
+                            <button onClick={this.startTimer}>Start timer</button>
+                        </td>
+                        <td>Time: {this.state.time}</td>
                     </tr>
                 </tbody>
             </table>
