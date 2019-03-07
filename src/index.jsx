92645db509c584d885c1f10ee8f9b9072fec2292
olofens <olofens@student.chalmers.se>
2019-03-07 17:20:22 +0100

    add react-router routing

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import myApp from './reducers';
 import App from './containers/app.jsx';
-import { init } from "./actions/index.js"
+import Login from "./containers/login.jsx";
+import Register from "./containers/register.jsx";
+import { init } from "./actions/index.js";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { devToolsEnhancer, composeWithDevTools } from 'redux-devtools-extension';
 
 
@@ -13,12 +16,20 @@ let store = createStore(myApp, composeWithDevTools(applyMiddleware(thunk)));
 store.dispatch(init());
 
 function render() {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById("app")
-  );
+    ReactDOM.render(
+
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            </BrowserRouter>
+
+        </Provider>,
+        document.getElementById("app")
+    );
 }
 
 store.subscribe(render);
