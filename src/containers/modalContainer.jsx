ccffa7b245ae4f903612337855677752d2423a9c
Strid <stride@student.chalmers.se>
2019-03-11 20:45:37 +0100

    add modal styling + better formik structure

--- src/containers/modalContainer.jsx
+++ src/containers/modalContainer.jsx
@@ -6,6 +6,7 @@ import { showModal, hideModal } from "../actions/index.js"
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import styles from './modalStyling.css'
+import './modal.css';
 
 import Modal from 'react-responsive-modal';
 //import Modal from 'react-bootstrap/Modal';
@@ -23,90 +24,133 @@ const customStyles = {
         border: '1px solid #ccc', background: '#fff',
         overflow: 'auto', WebkitOverflowScrolling: 'touch',
         borderRadius: '4px', outline: 'none', padding: '20px',
-        top: '50%', left: '50%', right: 'auto', bottom: 'auto',
-        marginRight: '-50%', transform: 'translate(-50%, -50%)',
-      }
-  };
+        top: '180px', left: '150px', right: '150px', bottom: '190px', position: 'absolute',
+
+    }
+};
 
 
-  const addTaskSchema = Yup.object().shape({
+const addTaskSchema = Yup.object().shape({
     title: Yup.string()
-      .required('Required'),
-    description: Yup.string() 
-      .required('Required'),
-    estimatedTime: Yup.string()
-      .required('Required'),
-  });
-  
-  const AddTaskForm = () => (
+        .required('Required'),
+    description: Yup.string()
+        .required('Required'),
+    estimatedTime: Yup.number()
+        .required('Required')
+        .positive('Time must be > 0')
+        .integer('Time must be integer value'),
+        
+});
+
+const AddTaskForm = () => (
     <div>
-      <h2>Add New Task</h2>
-      <Formik
-          initialValues={{
-              title: '',
-             description: '',
-             estimatedTime: ''
-          }}
-          validationSchema={addTaskSchema}
-          onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                console.log(JSON.stringify(values, null, 2));
-                fetch("http://127.0.0.1:3000/add_task", {
-    method: "post",
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-  
-    //make sure to serialize your JSON body
-    body: JSON.stringify(values, null, 2)
-  })
-  
-                setSubmitting(false);
-              }, 500);
+        <h1>Add New Task</h1>
+        <Formik
+            initialValues={{
+                title: '',
+                description: '',
+                estimatedTime: ''
             }}
-  
-          render={formProps => {
-            return(
-              <Form>
-                  <p>
-                  <Field 
-                   type="text" 
-                   name="title" 
-                   placeholder="Title" 
-                 /> 
-                 <ErrorMessage name="title" />
-                  </p>
-              
-                  <p>
-                  <Field 
-                    type="text" 
-                    name="estimatedTime" 
-                    placeholder="Estimated Time" 
-                           /> 
-                  <ErrorMessage name="estimatedTime" />
-                  </p>
-  
-                  <p>
-                  <Field component="textarea"
-                    type="text" 
-                    name="description" 
-                    placeholder="Task Description" 
-                           /> 
-                  <ErrorMessage name="description" />
-                  </p>
-          
-          <button 
-          type="submit" >
-          Add Task
-          </button>
-                   
-                </Form>
-             );
-          }}
-       />
+            validationSchema={addTaskSchema}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+
+                    console.log(JSON.stringify(values, null, 2));
+                    fetch("http://127.0.0.1:3000/add_task", {
+                        method: "post",
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(values, null, 2)
+                    })
+                    //TODO
+                    //this.props.hideModal
+                    //Göm modal här, vet inte hur
+                    setSubmitting(false);
+                }, 500);
+            }}
+
+            render={props => {
+                const {
+                    values,
+                    touched,
+                    errors,
+                    dirty,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    handleReset,
+                } = props;
+                return (
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="title" style={{ display: 'block' }}>
+                            Title
+                </label>
+                        <input
+                            id="title"
+                            placeholder="Enter your title"
+                            type="text"
+                            value={values.title}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={
+                                errors.title && touched.title ? 'text-input error' : 'text-input'
+                            }
+                        />
+                        {errors.title &&
+                            touched.title && <div className="input-feedback">{errors.title}</div>}
+                        <label htmlFor="description" style={{ display: 'block' }}>
+                            Description
+            </label>
+                        <input
+                            id="description"
+                            placeholder="Enter your description"
+                            type="text"
+                            value={values.description}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={
+                                errors.description && touched.description ? 'text-input error' : 'text-input'
+                            }
+                        />
+                        {errors.description &&
+                            touched.description && <div className="input-feedback">{errors.description}</div>}
+                        <label htmlFor="estimatedTime" style={{ display: 'block' }}>
+                            Estimated Time
+            </label>
+                        <input
+                            id="estimatedTime"
+                            placeholder="Enter your estimatedTime"
+                            type="text"
+                            value={values.estimatedTime}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={
+                                errors.estimatedTime && touched.estimatedTime ? 'text-input error' : 'text-input'
+                            }
+                        />
+                        {errors.estimatedTime &&
+                            touched.estimatedTime && <div className="input-feedback">{errors.estimatedTime}</div>}
+                        <button
+                            type="button"
+                            className="modalButton"
+                            onClick={handleReset}
+                            disabled={!dirty || isSubmitting}
+                        >
+                            Reset
+            </button>
+                        <button type="submit" className="modalButton">
+                            Submit
+            </button>
+
+                    </form>
+                );
+            }}
+        />
     </div>
-  );
+);
 
 
 class ModalContainer extends React.Component {
@@ -114,26 +158,19 @@ class ModalContainer extends React.Component {
         super(props);
     }
 
-    
-
     render() {
         return (
-            <div className="Modal">
+            <div className="modalApp">
                 <Modal
                     open={this.props.modalIsOpen}
                     onClose={this.props.hideModal}
                     center
                     focusTrapped
-                    classNames={{
-                        overlay: styles.customOverlay,
-                        modal: styles.customModal,
-                    }}
-                    >
-                    <h2>
-                        <AddTaskForm/>
-                    </h2>
+                    styles={customStyles}
+                >
+                    <AddTaskForm />
                 </Modal>
-           </div> 
+            </div>
         );
     }
 }
@@ -145,8 +182,10 @@ function mapStateToProps(state) {
 }
 
 function matchDispatchToProps(dispatch) {
-    return bindActionCreators({ showModal: showModal,
-                                hideModal: hideModal }, dispatch)
+    return bindActionCreators({
+        showModal: showModal,
+        hideModal: hideModal
+    }, dispatch)
 }
 
 export default connect(mapStateToProps, matchDispatchToProps)(ModalContainer);
