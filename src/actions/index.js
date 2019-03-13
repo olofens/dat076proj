24d6e0f92e735586dc62cce3fd775e458c37e39f
Gabriel Wallin <walling@student.chalmers.se>
2019-03-13 14:09:47 +0100

    Enable done tasks to spawn in right column on init

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,82 +1,91 @@
 export const startEditTask = () => {
-    return {
-        type: "START_EDIT_TASK",
-    }
-}
+  return {
+    type: "START_EDIT_TASK"
+  };
+};
 
 export const finishEditTask = (task, column) => {
-    return {
-        type: "FINISH_EDIT_TASK",
-        payload: {task: task, column: column}
-    }
-}
+  return {
+    type: "FINISH_EDIT_TASK",
+    payload: { task: task, column: column }
+  };
+};
 
+export const doingClickBack = task => {
+  return {
+    type: "DOING_CLICK_BACK",
+    payload: { task: task }
+  };
+};
 
+export const doingClickForward = task => {
+  return {
+    type: "DOING_CLICK_FORWARD",
+    payload: { task: task }
+  };
+};
 
-export const doingClickBack = (task) => {
-    return {
-        type: 'DOING_CLICK_BACK',
-        payload: {task: task}
-    }
-}
-
-export const doingClickForward = (task) => {
-    return {
-        type: 'DOING_CLICK_FORWARD',
-        payload: {task: task}
-    }
-}
-
-export const doneClick = (task) => {
-    return {
-        type: 'DONE_CLICK',
-        payload: {task: task}
-    }
-}
+export const doneClick = task => {
+  return {
+    type: "DONE_CLICK",
+    payload: { task: task }
+  };
+};
 
 export const showModal = () => {
-    return {
-        type: 'SHOW_MODAL'
-        
-    }
-}
+  return {
+    type: "SHOW_MODAL"
+  };
+};
 
 export const hideModal = () => {
-    return {
-        type: 'HIDE_MODAL'
-        
-    }
-}
-
-
+  return {
+    type: "HIDE_MODAL"
+  };
+};
 
 export const init = () => {
-    return(dispatch) => {
-        return fetch("http://127.0.0.1:3000/tasks").then(response => response.json()) .then((responseData) => {
-            dispatch(updateTasks(responseData));
-        })
-    }
-}
+  return dispatch => {
+    return fetch("http://127.0.0.1:3000/tasks")
+      .then(response => response.json())
+      .then(responseData => {
+        dispatch(updateTasks(responseData));
+      });
+  };
+};
 
-export const updateTasks = (data) => {
-    console.log(data);
-    return {
-        type: "INIT",
-        todoTasks: data
-    }
-}
+export const updateTasks = data => {
+  console.log(data);
+  const array1 = data;
+  const todoArray = array1.filter(task => {
+    return task.datefinished === null;
+  });
+  const doneArray = array1.filter(task => {
+    return task.datefinished !== null;
+  });
+  console.log("Todoarray: ");
+  console.log(todoArray);
+  console.log("Donearray: ");
+  console.log(doneArray);
+
+  return {
+    type: "INIT",
+    todoTasks: todoArray,
+    doneTasks: doneArray
+  };
+};
 
 export const dragTask = (id, column) => {
-    console.log("drag");
-    return {
-        type: "DRAG",
-        payload: {id: id, column: column}
-    }
-}
+  console.log("drag");
+  return {
+    type: "DRAG",
+    payload: { id: id, column: column }
+  };
+};
 
 export const dropTask = (id, columnFrom, columnTo) => {
-    return {
-        type: "DROP", 
-        payload: {id: id, columnFrom: columnFrom, columnTo:columnTo}
-    }
-}
+  return {
+    type: "DROP",
+    payload: { id: id, columnFrom: columnFrom, columnTo: columnTo }
+  };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,123 +1,132 @@
 const initialState = {
-    todoTasks: [],
-    doingTasks: [],
-    doneTasks: [],
-    heldTasks: [],
-    modalIsOpen: false, 
-    editModal: false
-}
+  todoTasks: [],
+  doingTasks: [],
+  doneTasks: [],
+  heldTasks: [],
+  modalIsOpen: false,
+  editModal: false
+};
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case "START_EDIT_TASK":
-            return Object.assign({}, state, {
-                editModal: true
-            })
-        
-        case "FINISH_EDIT_TASK":
-            // testing update. just setting description as of now.
-            action.payload.task.description = "EDITED";
-            
-            var col = getArray(action.payload.column, state);
-            
-            col[action.payload.task] = action.payload.task;
-            
-            return Object.assign({}, state, {
-                editModal: false,
-                [action.payload.column]: col
-            })
+  switch (action.type) {
+    case "START_EDIT_TASK":
+      return Object.assign({}, state, {
+        editModal: true
+      });
 
+    case "FINISH_EDIT_TASK":
+      // testing update. just setting description as of now.
+      action.payload.task.description = "EDITED";
 
-        case 'DOING_CLICK_BACK':
-            return Object.assign({}, state, {
-                doingTasks: state.doingTasks.filter((task) => task.id !== action.payload.task.id),
-                todoTasks: state.todoTasks.concat([action.payload.task])
-            })
+      var col = getArray(action.payload.column, state);
 
-        case 'DOING_CLICK_FORWARD':
-            return Object.assign({}, state, {
-                doingTasks: state.doingTasks.filter((task) => task.id !== action.payload.task.id),
-                doneTasks: state.doneTasks.concat([action.payload.task])
-            })
+      col[action.payload.task] = action.payload.task;
 
-        case 'DONE_CLICK':
-            return Object.assign({}, state, {
-                doneTasks: state.doneTasks.filter((task) => task.id !== action.payload.task.id),
-                doingTasks: state.doingTasks.concat([action.payload.task])
-            })
+      return Object.assign({}, state, {
+        editModal: false,
+        [action.payload.column]: col
+      });
 
-        case 'SHOW_MODAL':
-            return Object.assign({}, state, {
-                modalIsOpen : true
-        })
+    case "DOING_CLICK_BACK":
+      return Object.assign({}, state, {
+        doingTasks: state.doingTasks.filter(
+          task => task.id !== action.payload.task.id
+        ),
+        todoTasks: state.todoTasks.concat([action.payload.task])
+      });
 
-        case 'HIDE_MODAL':
-            return Object.assign({}, state, {
-                modalIsOpen : false
-        })
+    case "DOING_CLICK_FORWARD":
+      return Object.assign({}, state, {
+        doingTasks: state.doingTasks.filter(
+          task => task.id !== action.payload.task.id
+        ),
+        doneTasks: state.doneTasks.concat([action.payload.task])
+      });
 
-        case 'INIT':
-            return Object.assign({}, state, {
-                todoTasks: action.todoTasks
-            })
+    case "DONE_CLICK":
+      return Object.assign({}, state, {
+        doneTasks: state.doneTasks.filter(
+          task => task.id !== action.payload.task.id
+        ),
+        doingTasks: state.doingTasks.concat([action.payload.task])
+      });
 
-        case 'DRAG': // not used atm
-            if (action.payload.column == "LEFT") {
-                return Object.assign({}, state, {
-                    heldTasks: state.todoTasks.filter((task) => task.id === parseInt(action.payload.id))
-                })
-            }
+    case "SHOW_MODAL":
+      return Object.assign({}, state, {
+        modalIsOpen: true
+      });
 
-            else if (action.payload.column == "MIDDLE") {
-                return Object.assign({}, state, {
-                    heldTasks: state.doingTasks.filter((task) => task.id === parseInt(action.payload.id))
-                })
-            }
+    case "HIDE_MODAL":
+      return Object.assign({}, state, {
+        modalIsOpen: false
+      });
 
-            else if (action.payload.column == "RIGHT") {
-                return Object.assign({}, state, {
-                    heldTasks: state.doneTasks.filter((task) => task.id === parseInt(action.payload.id))
-                })
-            }
+    case "INIT":
+      return Object.assign({}, state, {
+        todoTasks: action.todoTasks,
+        doneTasks: action.doneTasks
+      });
 
-        case 'DROP':
-            var fromCol = getArray(action.payload.columnFrom, state);
-            var toCol = getArray(action.payload.columnTo, state);
-            var movedTask = fromCol.filter((task) => task.id === parseInt(action.payload.id));
+    case "DRAG": // not used atm
+      if (action.payload.column == "LEFT") {
+        return Object.assign({}, state, {
+          heldTasks: state.todoTasks.filter(
+            task => task.id === parseInt(action.payload.id)
+          )
+        });
+      } else if (action.payload.column == "MIDDLE") {
+        return Object.assign({}, state, {
+          heldTasks: state.doingTasks.filter(
+            task => task.id === parseInt(action.payload.id)
+          )
+        });
+      } else if (action.payload.column == "RIGHT") {
+        return Object.assign({}, state, {
+          heldTasks: state.doneTasks.filter(
+            task => task.id === parseInt(action.payload.id)
+          )
+        });
+      }
 
-            return Object.assign({}, state, {
-                [action.payload.columnFrom]: fromCol.filter((task) => task.id !== parseInt(action.payload.id)),
-                [action.payload.columnTo]: toCol.concat(movedTask)
-            })
+    case "DROP":
+      var fromCol = getArray(action.payload.columnFrom, state);
+      var toCol = getArray(action.payload.columnTo, state);
+      var movedTask = fromCol.filter(
+        task => task.id === parseInt(action.payload.id)
+      );
 
-        default:
-            return state
-    }
-}
+      return Object.assign({}, state, {
+        [action.payload.columnFrom]: fromCol.filter(
+          task => task.id !== parseInt(action.payload.id)
+        ),
+        [action.payload.columnTo]: toCol.concat(movedTask)
+      });
 
-
-
+    default:
+      return state;
+  }
+};
 
 function getArray(name, state) {
-    switch (name) {
-        case 'todoTasks':
-            console.log("hello");
-            return state.todoTasks
-        case 'doingTasks':
-            return state.doingTasks
-        case 'doneTasks':
-            return state.doneTasks
-        default:
-            return null
-    }
+  switch (name) {
+    case "todoTasks":
+      console.log("hello");
+      return state.todoTasks;
+    case "doingTasks":
+      return state.doingTasks;
+    case "doneTasks":
+      return state.doneTasks;
+    default:
+      return null;
+  }
 }
 
 function findIndex(array, id) {
-    console.log("given array " + JSON.stringify(array))
-    for (var i = 0; i < array.length; i++) {
-        if (array[i].id === id) {
-            return i;
-        }
+  console.log("given array " + JSON.stringify(array));
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].id === id) {
+      return i;
     }
-    return null;
+  }
+  return null;
 }
