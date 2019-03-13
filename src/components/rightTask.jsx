32b64e886d6ab5917893a3d68878960a7c397e3b
Strid <stride@student.chalmers.se>
2019-03-13 14:10:30 +0100

    fix modal close on add and improve sql queries

--- src/components/rightTask.jsx
+++ src/components/rightTask.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./righttask.css";
-import { getSQLdate } from "./dateConverter.js";
+//import { getSQLdate } from "./dateConverter.js";
 
 class RightTask extends React.Component {
   constructor(props) {
