2885de1211ae76e788f9874ad58cb46551d3debb
Gabriel Wallin <walling@student.chalmers.se>
2019-03-13 16:47:50 +0100

    Fix css (kind of, fix the rest later)

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import Header from "./header.jsx";
-import BoardContainer from "./boardContainer.jsx"
-import TaskListContainer from "./taskListContainer.jsx"
-import DoneListContainer from "./doneListContainer.jsx"
-import "./test.css"
-import ModalContainer from './modalContainer.jsx';
-import EditTaskModal from "./../modals/editTaskModal.jsx"
+import BoardContainer from "./boardContainer.jsx";
+import TaskListContainer from "./taskListContainer.jsx";
+import DoneListContainer from "./doneListContainer.jsx";
+import "./test.css";
+import ModalContainer from "./modalContainer.jsx";
+import EditTaskModal from "./../modals/editTaskModal.jsx";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="container">
-                <div className="itemHeader"> <Header /> </div>
-                <div className="itemSidebar1"><TaskListContainer /></div>
-                <div className="itemSidebar2"><DoneListContainer /></div>
-                <div className="itemContent"><BoardContainer /></div>
-                <div className="modalContainer"><ModalContainer /></div>
-                <div className="modalContainer"><EditTaskModal/></div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="appTest">
+        <div className="container">
+          <div className="itemHeader">
+            {" "}
+            <Header />{" "}
+          </div>
+          <div className="itemSidebar1">
+            <TaskListContainer />
+          </div>
+          <div className="itemSidebar2">
+            <DoneListContainer />
+          </div>
+          <div className="itemContent">
+            <BoardContainer />
+          </div>
+        </div>
+
+        <div className="modalContainer">
+          <ModalContainer />
+        </div>
+        <div className="modalContainer">
+          <EditTaskModal />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
